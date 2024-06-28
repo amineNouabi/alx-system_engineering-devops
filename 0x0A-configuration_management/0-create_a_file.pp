904c857c2affae1af7289d96a711082815155e5b
amineNouabi <aminenouabi9@gmail.com>
2024-06-28 16:30:59 +0100

    first puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@ file {
     'school':
     path => '/tmp/school',
     ensure => file,
-    content => "I love Puppet";
+    content => 'I love Puppet',
     group => 'www_data',
     owner => 'www_data',
     permissions => '0744',
