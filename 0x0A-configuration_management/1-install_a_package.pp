2241b7db078a2949f6d8935dabbf16722315a47e
amineNouabi <aminenouabi9@gmail.com>
2024-06-28 17:08:03 +0100

    ppupet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
 # Install Flask version 2.1.0 package using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
