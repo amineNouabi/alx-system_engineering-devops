f6f36eba8d13901e8c66c4a92d7e6e0d10532115
amineNouabi <aminenouabi9@gmail.com>
2024-07-01 18:16:21 +0100

    pop

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,12 @@
 # ssh setup for the server
-file { '/etc/ssh/sshd_config':
-  ensure => present,
-  content => template('ssh/sshd_config.erb'),
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
+file { 'Declare identity file':
+  path    => '/root/.ssh/config',
+  ensure  => file,
+  content => "Host 273190-web-01\n  HostName 273190-web-01\n  IdentityFile ~/.ssh/school\n  IdentitiesOnly yes\n",
+}
+
+file { 'Turn off password authentication':
+  path    => '/etc/ssh/sshd_config',
+  ensure  => file,
+  content => "PasswordAuthentication no\n",
 }
