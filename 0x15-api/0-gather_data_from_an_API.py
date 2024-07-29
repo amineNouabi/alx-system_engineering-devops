bf32e04e6e4740d8bdbebeaca147a901ae577209
amineNouabi <aminenouabi9@gmail.com>
2024-07-29 23:26:41 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,6 @@
 
 from requests import api
 from sys import argv
-from json import loads
 
 API_URL = "https://jsonplaceholder.typicode.com"
 
@@ -13,16 +12,16 @@ if __name__ == "__main__":
     todos = api.get("{}/todos?userId={}".format(API_URL, USER_ID)).json()
     employee = api.get("{}/users/{}".format(API_URL, USER_ID)).json()
 
-    name = employee["name"]
+    name = employee.get("title")
     completed_count = 0
     total_count = len(todos)
 
     for todo in todos:
-        if todo["completed"]:
+        if todo.get("completed"):
             completed_count += 1
 
     print("Employee {} is done with ({}/{}):".format(name,
           completed_count, total_count))
     for todo in todos:
-        if todo["completed"]:
-            print("\t {}".format(todo["title"]))
+        if todo.get("completed"):
+            print("\t {}".format(todo.get("title")))
