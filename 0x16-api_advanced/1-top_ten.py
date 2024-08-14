7f59d1a6ab91ccdbdd32780951e402944c90e29f
amineNouabi <aminenouabi9@gmail.com>
2024-08-14 19:08:08 +0100

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module defining number_of_subscribers function
+
+"""
+from requests import get
+
+BASE_URL = 'https://www.reddit.com/'
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
+
+def top_ten(subreddit):
+    """Queries the Reddit API and returns the number of subscribers"""
+
+    response = get("{}r/{}/hot.json?limit=10".format(BASE_URL, subreddit),
+                   headers=HEADERS, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json().get('data').get('children')
+    for post in data:
+        print(post.get('data').get('title'))
