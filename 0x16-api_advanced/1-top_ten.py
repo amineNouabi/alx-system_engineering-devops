f3d6741407c244bf8b484c4074180f57e259fff2
amineNouabi <aminenouabi9@gmail.com>
2024-08-15 12:42:17 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,8 +13,8 @@ def top_ten(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     headers = requests.utils.default_headers()
     headers.update(HEADERS)
-    response = requests.get("{}r/{}/hot.json?limit=10".format(BASE_URL,
-                                                              subreddit),
+    response = requests.get("{}r/{}/hot.json?limit=9".format(BASE_URL,
+                                                             subreddit),
                             headers=headers, allow_redirects=False)
     if response.status_code / 100 != 2:
         print(None)
