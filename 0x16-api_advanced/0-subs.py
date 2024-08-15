99ebfabbf6071d409398c2ef5e2848f05df0b47d
amineNouabi <aminenouabi9@gmail.com>
2024-08-15 12:22:12 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,6 +13,6 @@ def number_of_subscribers(subreddit):
 
     response = requests.get("{}r/{}/about.json".format(BASE_URL,
                             subreddit), allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
-    return int(response.json().get('data').get('subscribers'))
+    return response.json().get('data').get('subscribers')
