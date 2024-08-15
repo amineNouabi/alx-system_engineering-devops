f76a3201790a3f7c448e574b04dc683a994322de
amineNouabi <aminenouabi9@gmail.com>
2024-08-15 12:36:11 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Module defining number_of_subscribers function
-
 """
+
 import requests
 
 BASE_URL = 'https://www.reddit.com/'
@@ -11,9 +11,16 @@ HEADERS = {'User-Agent': 'My Custom one'}
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
-
+    headers = requests.utils.default_headers()
+    headers.update(HEADERS)
     response = requests.get("{}r/{}/about.json".format(BASE_URL,
-                            subreddit), headers=HEADERS, allow_redirects=False)
-    if response.status_code != 200:
+                            subreddit), headers=headers, allow_redirects=False)
+    if response.status_code // 100 != 2:
         return 0
-    return int(response.json().get('data').get('subscribers'))
+    try:
+        count = response.json().get('data').get('subscribers')
+        if not count:
+            return 0
+        return count
+    except Exception:
+        return 0
