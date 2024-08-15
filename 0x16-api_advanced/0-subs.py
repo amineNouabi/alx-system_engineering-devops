3291c2273fc3e9bcf7dd3250c104106b687e46e2
amineNouabi <aminenouabi9@gmail.com>
2024-08-15 12:18:55 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,13 @@ Module defining number_of_subscribers function
 import requests
 
 BASE_URL = 'https://www.reddit.com/'
-HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
 
     response = requests.get("{}r/{}/about.json".format(BASE_URL,
-                            subreddit), headers=HEADERS, allow_redirects=False)
+                            subreddit), allow_redirects=False)
     if response.status_code != 200:
         return 0
     return int(response.json().get('data').get('subscribers'))
